e8673b4a99e5157c853612d70165565d03e1a2bb
Malesandr <aleksandr.mikolenko@gmail.com>
2024-01-15 23:49:07 +0200

    Task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,51 @@
+'use strict';
+
 import iziToast from "izitoast";
+
+  
+  document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+  const form = document.querySelector('.form');
+  
+  const delayValue = document.querySelector('[name="delay"]');
+  const stateValue = document.querySelectorAll('[name="state"]');
+  
+  const delay = parseInt(delayValue.value);
+  const state = Array.from(stateValue).find(input => input.checked).value;
+  
+  const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    form.reset()
+  
+    promise
+      .then(() => {
+        iziToast.show({
+          message: `✅ Fulfilled promise in ${delay} ms`,
+          messageColor: "#FFF",
+          messageSize: "16px",
+          position: "topRight",
+          backgroundColor: "#59A10D",
+          close: false,
+        });
+      })
+      .catch(() => {
+        iziToast.show({
+          message: `❌ Rejected promise in ${delay} ms`,
+          messageColor: "#FFF",
+          messageSize: "16px",
+          position: "topRight",
+          backgroundColor: "#EF4040",
+          close: false
+        });
+      });
+
+  });
